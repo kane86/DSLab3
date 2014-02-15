15b91203bebdf5f897bc950a9fd86cc05807f427
ankursha <ankursha@andrew.cmu.edu>
2014-02-15 03:04:09 -0500

    More infra changes

--- DSLab3/src/message/ResourceMsgPasser.java
+++ DSLab3/src/message/ResourceMsgPasser.java
@@ -14,6 +14,7 @@ public class ResourceMsgPasser implements Runnable {
 	
 	public MessagePasser msgPass;
     HashMap<String, VotingSet> votingSets;
+    private ArrayList<Resource> resources;
     Thread receiveThread;
     
 	public ResourceMsgPasser(String configuration_filename, String local_name) throws IOException {
@@ -24,6 +25,8 @@ public class ResourceMsgPasser implements Runnable {
 		votingSets = new HashMap<String, VotingSet>();	
 		
 		/* TODO: Populate the Voting Sets */
+		
+		resources = new ArrayList<Resource>();
 		
 		/* Receive thread */
 		receiveThread = new Thread(this, "Multicast Message receive thread");
@@ -40,7 +43,66 @@ public class ResourceMsgPasser implements Runnable {
 			rcvdMsg = (VotingMessage)(msgPass.receive());
 			if (rcvdMsg != null) {
 				System.out.println("Rcvd VotingMsg: " + rcvdMsg.toString());
+				switch (rcvdMsg.getVotingMsgType()) {
+
+				case Req:
+					processReqMsg(rcvdMsg);
+					break;
+
+				case Ack:
+					processAckMsg(rcvdMsg);
+					break;
+					
+				default:
+					System.out.println("[DBG_RUN]: Invalid MsgType");
+				}
 			}
 		}
+	}
+
+	public void getResource(String resourceName) {
+		/* send a request to the voting set */
+		/* TODO: After parsing code */
+	}
+	
+	private void processReqMsg(VotingMessage rcvdMsg){
+	}
+
+	private void processAckMsg(VotingMessage rcvdMsg){
+	}
+
+	private void multCast(String grpName, String resName, String kind, boolean log, Object data) {
+		
+		VotingSet votingSet;
+
+		/* TODO Get Voting Set */
+		votingSet = votingSets.get(grpName);
+		ArrayList<String> members = votingSet.getMembers();
+		if (members == null) {
+			System.out.println("[ERR_multCast]: Empty Group");
+			return;
+		}
+		System.out.println("[DBG_multCast]: Enter: grpName: " + grpName);
+
+		for (String dest : members) {
+			VotingMessage votingMsg = new VotingMessage(msgPass.GetLocalName(), grpName, resName, dest, 
+														"Resource", false, null, msgPass.GetClock().GetTimeStamp());
+			votingMsg.setVotingMsgType(VotingMessageType.Req);
+			msgPass.GetClock().Update();
+			msgPass.send(votingMsg);
+		}
+	}
+	
+	private Resource searchResource(String name) {
+		Resource retResource = null;
+
+		for (Resource iter: resources) {
+			if (iter.getName().equals(name)) {
+				retResource = iter;
+				break;
+			}
+		}
+
+		return retResource;
 	}
 }
